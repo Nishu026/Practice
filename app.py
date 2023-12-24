520b6d4664af3c83de5d2bc894819b895a52a725
Nishu026 <pmnishu.2000@gmail.com>
2023-12-24 20:21:03 +0530

    added line of code

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
+printf("Addition of two numbers:")
 num1 = int(input("Enter 1st number:"))
 num2 = int(input("Enter 2nd number:"))
 addition= num1+num2
-print(f"The addition of {num1} + {num2} = {addition}")
+print(f"The addition of {num1} + {num2} = {addition}"
