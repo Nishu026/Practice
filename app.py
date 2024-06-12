5bd831648686e134d8ed806ea457bc7ae216296e
Nishu026 <pmnishu.2000@gmail.com>
2024-06-12 20:37:46 +0530

    line of code added

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ num1 = int(input("Enter 1st number:"))
 num2 = int(input("Enter 2nd number:"))
 addition= num1+num2
 print(f"The addition of {num1} + {num2} = {addition}")
+
+age= int(input("Enter your age"))
+name = input("Enter your name")
+print(f"your name is {name} and age is {age}")
